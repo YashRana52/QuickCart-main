c31a18798646395ed3c5f309d98e4047e5ea4265
yash rana <2200520100072@gmail.com>
2025-08-16 22:27:57 +0530

    userjs

--- models/user.js
+++ models/user.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    _id: { type: String, required: true },
+    clerkId: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true },
     imageUrl: { type: String, required: true },
     cartItems: { type: Object, default: {} },
   },
-  { minimize: false }
+  { minimize: false, timestamps: true }
 );
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
